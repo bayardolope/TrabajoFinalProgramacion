6c15a33039364e53428b8eebb06ddfc0a166c006
andreasalg <165739594+andreasalg@users.noreply.github.com>
2024-07-02 00:00:23 -0600

    Ingresos

--- variables.h
+++ variables.h
@@ -1,12 +1,9 @@
 #define MAX_REG 1000
 
-struct Venta {
-    int numeroFactura;
-    char nombreCliente[50];
-    char nombreVendedor[50];
-    int dia;
-    int mes;
-    int cantidadPollos;
-    float precioPorPollo;
-    float total;
+typedef struct FACTURA {
+    int id;
+    char cliente[30];
+    double cantidad;
+    double precio;
+    char fecha[20];
 };
